662f2caf3b27bbc109d4ddc3693250a467ab8c6f
Hannah VanWingen-Eckertova <your-email@example.com>
2024-07-11 16:43:54 -0400

    added parse functionality to api view

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -14,11 +14,28 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        try: 
+            address_string = request.query_params.get('address', '')
+            if not address_string:
+                raise ParseError("No address provided.")
+
+            parsed_address = self.parse(address_string)
+            return Response(
+                {
+                    "input_string": address_string, 
+                    "address_components": parsed_address['address_components'], 
+                    "address_type": parsed_address['address_type']
+                    }
+                )
+
+        except Exception as e:
+            return Response(
+                {"error": "Could not parse address. Error: {}".format(str(e))},
+                status=500
+            )
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
-        return address_components, address_type
+        # using usaddress: https://github.com/datamade/usaddress
+        tagged_address = usaddress.tag(address)
+        address_components, address_type = tagged_address
+        return {"address_components": address_components, "address_type": address_type}
